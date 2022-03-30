5f8d0f4e19cbbfd1687478ea8a399b93dc725400
hello157412 <81363178+hello157412@users.noreply.github.com>
2022-03-30 07:19:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
-noseX=0;
-noseY=0;
+leftWrist_x = 0;
+rightWrist_x = 0;
 difference = 0;
-rightWristX = 0;
-leftWristX = 0;
 
-  function setup() {
-  video = createCapture(VIDEO);
-  video.size(550, 500);
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
 
-  canvas = createCanvas(550, 550);
-  canvas.position(560,150);
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
 
-  poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
 }
 
-function modelLoaded() {
-  console.log('PoseNet Is Initialized!');
+function draw(){
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Dani',50,400);
 }
 
-
-function gotPoses(results)
-{
-  if(results.length > 0)
-  {
-    console.log(results);
-    noseX = results[0].pose.nose.x;
-    noseY = results[0].pose.nose.y;
-    console.log("noseX = " + noseX +" noseY = " + noseY);
-
-    leftWristX = results[0].pose.leftWrist.x;
-    rightWristX = results[0].pose.rightWrist.x;
-    difference = floor(leftWristX - rightWristX);
-
-    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
-  }
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
 }
 
-function draw() {
-background('#969A97');
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
 
-  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
-  fill('#F90093');
-  stroke('#F90093');
-  square(noseX, noseY, difference);
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
 }
